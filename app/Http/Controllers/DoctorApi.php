7503b2186f7308181b2166447850a8ca3c99d273
mahesh mestri <mahesh.mestri@globalspace.co.in>
2023-08-29 13:10:30 +0530

    diganostic

--- app/Http/Controllers/DoctorApi.php
+++ app/Http/Controllers/DoctorApi.php
@@ -42,10 +42,10 @@ class DoctorApi extends Controller
             $save = $pm->save();
             if ($save) {
                 $currentTimestamp = $this->generateTimestamp();
-                DB::insert('insert into patient_doctor_relation (user_map_id, patient_id, visit_type, created_at) values(?,?,?,?)', [$esteblishmentusermapID, $pm->id, $input['visit_type'], $currentTimestamp]);
+                DB::insert('insert into patient_doctor_relation (user_map_id, patient_id, visit_type, created_at) values(?,?,?,?)', [$esteblishmentusermapID, $pm->patient_id, $input['visit_type'], $currentTimestamp]);
                 return response()->json(['status' => true, 'message' => 'Patient Registration Successful'], 200);
             } else {
-                return response()->json(['status' => false, 'message' => 'Patient Registration Failed'], 500);
+                return response()->json(['status' => false, 'error' => 'Something went wrong', 'message' => "Patient Registration Failed"], 500);
             }
         } catch (\Throwable $th) {
             dd($th);
@@ -59,17 +59,22 @@ class DoctorApi extends Controller
         // dd($entity);
         // doctor is exist and moving forward so tempory purpose
         $pm = new Patients();
-        return response()->json(['status' => 'success', 'data' => $pm->patientlist($esteblishmentusermapID)], 200);
+        $data = $pm->patientlist($esteblishmentusermapID);
+        if ($data) {
+            return response()->json(['status' => true, 'data' => $data], 200);
+        }
+        return response()->json(['status' => false, 'message' => 'No patient records found (and its depends on doctor id)']);
     }
 
     // diagnostic report
-    public function createDiagnosticReport($docId, Request $request)
+    public function createDiagnosticReport($docId, $patientId, Request $request)
     {
+        /* before creating diagnostric report we have to check the patient have doctor have relation */
         try {
             $req = $request->all();
             // dd($req, $docId);
             $dr = new DiagnosticReport();
-            $dr->patient_id = $req['patient_id'];
+            $dr->patient_id = $patientId;
             $dr->user_map_id = $docId;
             $dr->report_type = $req['report_type'];
             $dr->report_category = $req['report_category'];
@@ -77,129 +82,31 @@ class DoctorApi extends Controller
             $dr->report_conclusion = $req['report_conclusion'];
             $dr->reporting_Doctor = $req['reporting_Doctor'];
             $dr->upload_file = $req['upload_file'];
+            $dr->upload_file_name = $req['upload_file_name'];
             $dr->created_at = $req['report_created_date'];
-
             $save = $dr->save();
-            // dd($save);
             if ($save) {
                 return response()->json(['status' => true, 'message' => "Diagnostic report successfully added"], 200);
             }
         } catch (\Throwable $th) {
-
-            dd("catch", $th);
             return response()->json(["status" => false, 'message' => "Internal Server Error"], 500);
         }
     }
 
-    public function diagnosticlist($patientId)
+    public function getDiagnosticList($docId, $patientId)
     {
-        $api = new Patients();
-        $entity_exist = $api->patientExist($patientId);
-        if ($entity_exist) {
-            $fun = new DiagnosticReport();
-            $res = $fun->getDiagnosticReport($patientId);
-            return response()->json(['status' => true, "data" => $res], 200);
-        }
-        return response()->json(['status' => false, "message" => "Patient Not exist"], 404);
-    }
+        /* here first check doctor and patient is exist */
+        $patientModel = new Patients();
+        $patietntExist = $patientModel->patientExist($patientId);
 
-    // opconsultation
-    public function createOpConsultation($docId, Request $request)
-    {
-        try {
-            $req = $request->all();
-            $op = new Opconsultation();
-            $op->patient_id = $req['patient_id'];
-            $op->user_map_id = $docId;
-            $op->notes = $req['notes'];
-            $op->upload_file = $req['upload_file'];
-
-            $save = $op->save();
-            if ($save) {
-                return response()->json(['status' => true, 'message' => "Opconsultation record successfully added"], 200);
+        if ($patietntExist) {
+            $api = new DiagnosticReport();
+            $res = $api->getDiagnosticReport($docId, $patientId);
+            if ($res) {
+                return response()->json(['status' => true, "diagnostic_data" => $res], 200);
             }
-        } catch (\Throwable $th) {
-            dd($th);
-            return response()->json(["status" => false, 'message' => "Internal Server Error"], 500);
+            return response()->json(['status' => false, "message" => "Patient Records Not Available"], 400);
         }
-    }
-
-    public function Opconsultationlist($patientId)
-    {
-        // first we have to check the patient and doctor is exist or not
-        $api = new Patients();
-        $entity_exist = $api->patientExist($patientId);
-        if ($entity_exist) {
-            $fun = new Opconsultation();
-            $res = $fun->getOpconsultationReport($patientId);
-            return response()->json(['status' => true, "data" => $res], 200);
-        }
-        return response()->json(['status' => false, "message" => "Patient Not exist"], 404);
-    }
-
-    // Discharge summary
-    public function CreateDischargeSummary($docId, Request $request)
-    {
-        try {
-            $req = $request->all();
-            $op = new DischargeSummary();
-
-            $op->patient_id = $req['patient_id'];
-            $op->user_map_id = $docId;
-            $op->notes = $req['notes'];
-            $op->upload_file = $req['upload_file'];
-
-            $save = $op->save();
-            if ($save) {
-                return response()->json(['status' => true, 'message' => "Discharge summary record successfully added"], 200);
-            }
-        } catch (\Throwable $th) {
-            return response()->json(["status" => false, 'message' => "Internal Server Error"], 500);
-        }
-    }
-
-    public function DischargeSummaryList($patientId)
-    {
-        $api = new Patients();
-        $entity_exist = $api->patientExist($patientId);
-        if ($entity_exist) {
-            $fun = new DischargeSummary();
-            $res = $fun->getDischargeSummaryList($patientId);
-            return response()->json(['status' => true, "data" => $res], 200);
-        }
-        return response()->json(['status' => false, "message" => "Patient Not exist"], 404);
-    }
-
-    // Record prescription
-    public function UploadRecordPrescription($docId, Request $request)
-    {
-        try {
-            $req = $request->all();
-            $op = new RecordPrescription();
-            $op->patient_id = $req['patient_id'];
-            $op->user_map_id = $docId;
-            $op->notes = $req['notes'];
-            $op->upload_file = $req['upload_file'];
-
-            $save = $op->save();
-            if ($save) {
-                return response()->json(['status' => true, 'message' => "Record prescription successfully added"], 200);
-            }
-        } catch (\Throwable $th) {
-            dd($th);
-            return response()->json(["status" => false, 'message' => "Internal Server Error"], 500);
-        }
-    }
-
-    public function RecordPrescriptionList($patientId)
-    {
-        $api = new Patients();
-        $entity_exist = $api->patientExist($patientId);
-        if ($entity_exist) {
-            $fun = new RecordPrescription();
-            $res = $fun->getRecordPrescriptionList($patientId);
-            return response()->json(['status' => true, "data" => $res], 200);
-        }
-        return response()->json(['status' => false, "message" => "Patient Not exist"], 404);
+        return response()->json(['status' => false, "message" => "Patient Not Found"], 400);
     }
 }
